9c0e1aa5a5ba67a50cc9bf25fbb9d95b6ceafa46
chenqiang <393562632@qq.com>
2016-09-30 14:18:05 +0800

    back up

---
+++ src/main/java/com/study/framework/core/helper/Beanhelper.java
@@ -0,0 +1,48 @@
+package com.study.framework.core.helper;
+
+import com.study.framework.core.util.ReflectionUtil;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by jackeyChen on 2016/9/29.
+ */
+public final class Beanhelper {
+
+    private static final Map<Class<?>, Object> BEAN_MAP = new HashMap<Class<?>, Object>();
+
+    static {
+        Set<Class<?>> beanClassSet = ClassHelper.getBeanClassSet();
+        for (Class<?> beanClass : beanClassSet) {
+            Object obj = ReflectionUtil.newInstance(beanClass);
+            BEAN_MAP.put(beanClass, obj);
+        }
+    }
+
+    /**
+     * 获取Bean的映射
+     *
+     * @return
+     */
+    public static Map<Class<?>, Object> getBeanMap() {
+        return BEAN_MAP;
+    }
+
+    /**
+     * 获取Bean实例
+     *
+     * @param cls
+     * @param <T>
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T getBean(Class<T> cls) {
+        if(!BEAN_MAP.containsKey(cls)) {
+            throw new RuntimeException("can't get bean by class :" + cls);
+        }
+        return (T) BEAN_MAP.get(cls);
+    }
+}
---
+++ src/main/java/com/study/framework/core/helper/ClassHelper.java
@@ -0,0 +1,71 @@
+package com.study.framework.core.helper;
+
+import com.study.framework.core.annotation.Controller;
+import com.study.framework.core.annotation.Service;
+import com.study.framework.core.util.ClassUtil;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by jackeyChen on 2016/9/28.
+ */
+public final class ClassHelper {
+    private static final Set<Class<?>> CLASS_SET;
+
+    static {
+        String basePackage = ConfigHelper.getAppBasePackage();
+        CLASS_SET = ClassUtil.getClassSet(basePackage);
+    }
+
+    /**
+     * 获取应用包名下的所有类
+     *
+     * @return
+     */
+    public static Set<Class<?>> getClassSet() {
+        return CLASS_SET;
+    }
+
+    /**
+     * 获取应用报名下的所有service类
+     *
+     * @return
+     */
+    public static Set<Class<?>> getServiceClassSet() {
+        Set<Class<?>> classSet = new HashSet<Class<?>>();
+        for (Class<?> cls : CLASS_SET) {
+            if (cls.isAnnotationPresent(Service.class)) {
+                classSet.add(cls);
+            }
+        }
+        return classSet;
+    }
+
+    /**
+     * 获取应用包名下的所有Controller类
+     *
+     * @return
+     */
+    public static Set<Class<?>> getControllerClassSet() {
+        Set<Class<?>> classSet = new HashSet<Class<?>>();
+        for (Class<?> cls : CLASS_SET) {
+            if (cls.isAnnotationPresent(Controller.class)) {
+                classSet.add(cls);
+            }
+        }
+        return classSet;
+    }
+
+    /**
+     * 获取应用包名下的所有Bean类（包括： service Controller）
+     *
+     * @return
+     */
+    public static Set<Class<?>> getBeanClassSet() {
+        Set<Class<?>> beanClassSet = new HashSet<>();
+        beanClassSet.addAll(getServiceClassSet());
+        beanClassSet.addAll(getControllerClassSet());
+        return beanClassSet;
+    }
+}
