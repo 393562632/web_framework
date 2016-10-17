31fee361958c44850935f8fa2afb525897b52f97
chenqiang <393562632@qq.com>
2016-10-17 17:03:37 +0800

    back up

--- src/test/java/step3/ConfigHeplerTest.java
+++ src/test/java/step3/ConfigHeplerTest.java
@@ -1,14 +1,10 @@
 package step3;
 
-import com.study.app.step2.service.CustomerService;
-import com.study.framework.core.helper.ConfigHelper;
+
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.net.URL;
-import java.util.Enumeration;
-import java.util.Set;
 
 /**
  * Created by jackeyChen on 2016/9/27.
