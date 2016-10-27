2147c9a2f9995b2cbed4f9b14d1621f30b26d33f
chenqiang <393562632@qq.com>
2016-10-27 23:54:49 +0800

    finish

---
+++ aop/src/main/java/aop/aspectJ/annotion/GreetingImpl.java
@@ -0,0 +1,22 @@
+package aop.aspectJ.annotion;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by jackeyChen on 2016/10/26.
+ */
+@Component
+public class GreetingImpl implements Hello {
+    @Override
+    public void say(String name) {
+        System.out.println("say....." + name);
+    }
+
+    public void goodMorning(String name) {
+        System.out.println("goodMorning ....." + name);
+    }
+
+    public void goodNight(String name) {
+        System.out.println("goodAfterNight ....." + name);
+    }
+}
---
+++ aop/src/main/java/aop/aspectJ/annotion/Hello.java
@@ -0,0 +1,9 @@
+package aop.aspectJ.annotion;
+
+/**
+ * Created by jackeyChen on 2016/10/20.
+ */
+public interface Hello {
+
+    public void say(String name);
+}
